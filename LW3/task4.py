5c231f838ac7deb872ee9c8c1faf95ca10add28a
mgfirefox <ma2ks2im8ka228@gmail.com>
2023-10-06 22:27:22 +0300

    task 3.4

---
+++ LW3/task4.py
@@ -0,0 +1,26 @@
+import json
+
+
+def start():
+    total_profit = 0
+    profitable_firms_amount = 0
+    firms = []
+
+    with open("firms.txt") as file:
+        for line in file:
+            firm_data = line[:-1].split()
+            firms.append({firm_data[0]: float(firm_data[2]) - float(firm_data[3])})
+
+            if firms[-1][firm_data[0]] >= 0:
+                total_profit += firms[-1][firm_data[0]]
+                profitable_firms_amount += 1
+
+    if len(firms) == 0:
+        print("Файл пуст")
+        return
+
+    print("Средняя прибыль фирм:", total_profit / len(firms))
+    print(firms)
+
+    with open("firms.json", 'w') as file:
+        json.dump(firms, file)
