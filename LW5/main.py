a5320885da0e651f23cf75208d5bb126336459b5
mgfirefox <ma2ks2im8ka228@gmail.com>
2023-11-15 11:10:38 +0300

    main 5

---
+++ LW5/main.py
@@ -0,0 +1,23 @@
+import task1
+import task2
+import task3
+
+while True:
+    try:
+        user_choice = float(input("Введите номер задания (1.1-1.2, 2, 3.1-3.2) или 0 для выхода: "))
+    except ValueError:
+        print("Введите число 1.1, 1.2, 2, 3.1, 3.2 или 0")
+        continue
+
+    if user_choice == 0:
+        break
+    elif user_choice == 1.1:
+        task1.start1()
+    elif user_choice == 1.2:
+        task1.start2()
+    elif user_choice == 2:
+        task2.start()
+    elif user_choice == 3.1:
+        task3.start1()
+    elif user_choice == 3.2:
+        task3.start2()
