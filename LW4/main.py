b41b112f7f3b2d476839a1b249cf0806e328a4e8
mgfirefox <ma2ks2im8ka228@gmail.com>
2023-11-26 14:55:52 +0300

    main 4

--- LW4/main.py
+++ LW4/main.py
@@ -1,18 +1,20 @@
 import task2
 import library
 
-while True:
-    try:
-        user_choice = int(input("Введите номер задания (от 2 до 3) или 0 для выхода: "))
-    except ValueError:
-        print("Введите целое число от 2 до 3 или 0")
-        continue
 
-    if user_choice == 0:
-        break
-    elif user_choice == 2:
-        task2.start()
-    elif user_choice == 3:
-        library.Library.start()
-    else:
-        print("Введите целое число от 2 до 3 или 0")
+if __name__ == "__main__":
+    while True:
+        try:
+            user_choice = int(input("Введите номер задания (от 2 до 3) или 0 для выхода: "))
+        except ValueError:
+            print("Введите целое число от 2 до 3 или 0")
+            continue
+
+        if user_choice == 0:
+            break
+        elif user_choice == 2:
+            task2.start()
+        elif user_choice == 3:
+            library.Library.start()
+        else:
+            print("Введите целое число от 2 до 3 или 0")
