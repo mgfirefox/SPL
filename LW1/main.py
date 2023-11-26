e626b67b18b861e155e459d54214d550254eb5c9
mgfirefox <ma2ks2im8ka228@gmail.com>
2023-11-26 14:55:24 +0300

    main 1

--- LW1/main.py
+++ LW1/main.py
@@ -5,28 +5,30 @@ import task4
 import task5
 import task6
 
-while True:
-    try:
-        user_choice = int(input("Введите номер задания (от 1 до 6) или 0 для выхода: "))
-    except ValueError:
-        print("Введите целое число от 0 до 6")
-        continue
 
-    if user_choice < 0 or user_choice > 6:
-        print("Введите целое число от 0 до 6")
-        continue
+if __name__ == "__main__":
+    while True:
+        try:
+            user_choice = int(input("Введите номер задания (от 1 до 6) или 0 для выхода: "))
+        except ValueError:
+            print("Введите целое число от 0 до 6")
+            continue
 
-    if user_choice == 0:
-        break
-    elif user_choice == 1:
-        task1.start()
-    elif user_choice == 2:
-        task2.start()
-    elif user_choice == 3:
-        task3.start()
-    elif user_choice == 4:
-        task4.start()
-    elif user_choice == 5:
-        task5.start()
-    elif user_choice == 6:
-        task6.start()
+        if user_choice < 0 or user_choice > 6:
+            print("Введите целое число от 0 до 6")
+            continue
+
+        if user_choice == 0:
+            break
+        elif user_choice == 1:
+            task1.start()
+        elif user_choice == 2:
+            task2.start()
+        elif user_choice == 3:
+            task3.start()
+        elif user_choice == 4:
+            task4.start()
+        elif user_choice == 5:
+            task5.start()
+        elif user_choice == 6:
+            task6.start()
